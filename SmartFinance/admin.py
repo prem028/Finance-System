fd0569926fc7222cb8a7d16da33042448344c865
prem028 <22it127@charusat.edu.in>
2024-06-08 11:19:41 +0530

    first commit

---
+++ SmartFinance/admin.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from .models import *
+
+# Register your models here.
+admin.site.register(Categories)
+admin.site.register(Expense)
+admin.site.register(SignUp)
+admin.site.register(Contact)
+admin.site.register(Subscribe)
+admin.site.register(Faqs)
